df68dfe2f23c819dfe4e311efb7a06bee36d0cec
Sergei Sivtsev <sergykt@gmail.com>
2024-06-19 21:02:42 +0500

    feat: input password visibility toggle

--- src/shared/ui/Input/Input.tsx
+++ src/shared/ui/Input/Input.tsx
@@ -1,4 +1,5 @@
-import { type ComponentProps } from 'react';
+import { useCallback, useState, type ComponentProps } from 'react';
+import { BiHide } from 'react-icons/bi';
 import classNames from 'classnames';
 import styles from './Input.module.scss';
 
@@ -26,24 +27,38 @@ export const Input = (props: InputProps) => {
     autoComplete,
   } = props;
 
+  const [showPassword, setShowPassword] = useState(false);
+
+  const togglePasswordVisibility = useCallback(
+    () => setShowPassword((prev) => !prev),
+    [setShowPassword],
+  );
+
   return (
     <div className={classNames(styles.wrapper, className)}>
       <label className={styles.label} htmlFor={id}>
         {label}
       </label>
-      <input
-        className={styles.input}
-        id={id}
-        type={type}
-        placeholder={placeholder}
-        value={value}
-        name={name}
-        onBlur={onBlur}
-        onChange={onChange}
-        required={required}
-        disabled={disabled}
-        autoComplete={autoComplete}
-      />
+      <div className={styles.inputWrapper}>
+        <input
+          className={styles.input}
+          id={id}
+          type={type === 'password' && showPassword ? 'text' : type}
+          placeholder={placeholder}
+          value={value}
+          name={name}
+          onBlur={onBlur}
+          onChange={onChange}
+          required={required}
+          disabled={disabled}
+          autoComplete={autoComplete}
+        />
+        {type === 'password' && (
+          <button type='button' className={styles.showPassword} onClick={togglePasswordVisibility}>
+            <BiHide />
+          </button>
+        )}
+      </div>
       {error && <p className={styles.error}>{helperText}</p>}
     </div>
   );
